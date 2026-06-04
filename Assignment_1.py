acdd250a0662425d4b448fd437f732c44e8381c5
Harshita Sharma <08harshiiii@gmail.com>
2026-06-04 23:54:29 +0530

    Add files via upload

---
+++ Assignment_1.py
@@ -0,0 +1,16 @@
+# Multiple objects
+print("Name:", "Harshita", "Age:", 20)
+
+# Using separator (sep)
+print("Python", "Java", "C++", sep=" | ")
+
+# Using end
+print("Hello", end=" ")
+print("World!")
+
+# Combining sep and end
+print("2026", "06", "04", sep="-", end=" ")
+print("Date")
+
+name = input("Enter your name: ")
+print("Hello,", name + "!", "Welcome!")
